8ca37dc73310f4f4ec752ed241bb72d4ab87f402
Nemesis-Nocturnal-Ray963 <muzukiray963@gmail.com>
2025-09-22 02:00:02 +0900

    minecraft id Adaptation

--- tiktok_live_minecraft_for_october/modules/minecraft_interactive_command.py
+++ tiktok_live_minecraft_for_october/modules/minecraft_interactive_command.py
@@ -23,75 +23,91 @@ finish_time = 0
 async def command_send_queue(code):
     await cwm.command_queue.put(code)
 
-async def blank_info(user,giftname):
-    print(f"name:{user}  gift:{giftname}")
-async def heart_me(user,count):
+async def blank_info(user,giftname,minecraft_id):
+    print(f"name:{user}  gift:{giftname} stremar:{minecraft_id}")
+    print(minecraft_id)
+
+
+async def heart_me(user,count,minecraft_id):
     print(f"{user} send Heart Me...")
     await command_send_queue(f"bedrock tnt 3 {user}")
     await command_send_queue('title @a title {"text":"§cハートミー！Thx！"}')
     for i in range(count):
         await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
-async def spawn_tnt(user, count, delay=0.1):
+    print(minecraft_id)
+
+
+async def spawn_tnt(user, count, delay,minecraft_id):
     await command_send_queue('title @a title {"text":"1TNT"}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
     for i in range(count):
         await command_send_queue(f"bedrock tnt 1 {user}")
         await asyncio.sleep(delay)
+    print(minecraft_id)
 
-async def spawn_multi_tnt(user, count, per, delay):
+async def spawn_multi_tnt(user, count, per, delay,minecraft_id):
     await command_send_queue(f'title @a title {{"text":"{per}TNT"}}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
     for i in range(count):
         await command_send_queue(f"bedrock tnt {per} {user}")
         await asyncio.sleep(delay)
+    print(minecraft_id)
 
 
-async def spawn_donuts_tnt(user, count, per, delay):
+async def spawn_donuts_tnt(user, count, per, delay,minecraft_id):
     await command_send_queue(f'title @a title {{"text":"イ・ﾓｰｼｮﾅﾙ・ダメージ！"}}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
     for i in range(count):
         await command_send_queue(f"bedrock tnt {per} {user}")
         await asyncio.sleep(delay)
+    print(minecraft_id)
 
-async def summon_zombies(user, count):
+
+async def summon_zombies(user, count,minecraft_id):
     await command_send_queue('title @a title {"text":"ゾンビのお友達～"}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
     for z in range(count):
         for i in range(15):
             await command_send_queue(f'execute at @a run summon zombie ~ ~3 ~ {{IsBaby:0,ArmorItems:[{{}},{{}},{{}},{{id:"minecraft:carved_pumpkin",Count:1}}],ArmorDropChances:[0F,0F,0F,0F],CustomName:"{user}の分身",CustomNameVisible:1}}')
             await asyncio.sleep(0.05)
+    print(minecraft_id)
 
-async def levitation_effect(user,count,delay):
+async def levitation_effect(user,count,delay,minecraft_id):
     await command_send_queue('title @a title {"text":"浮遊"}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
     for i in range(count):
-        await command_send_queue(f"effect give @a minecraft:levitation {delay} 2")
+        await command_send_queue(f"execute at {minecraft_id} run effect give {minecraft_id} minecraft:levitation {delay} 2")
         await asyncio.sleep(delay)
+    print(minecraft_id)
 
-async def fill_blocks(user,count):
+async def fill_blocks(user,count,minecraft_id):
     await command_send_queue('title @a title {"text":"§c400ブロック埋めたて"}')
     await command_send_queue(f'title @a subtitle {{"text":"ありがとう、{user}"}}')
     for i in range(count):
         await command_send_queue("bedrock fillblock 200")
     await asyncio.sleep(1)
+    print(minecraft_id)
 
-async def fill_area(user):
+async def fill_area(user,minecraft_id):
     await command_send_queue('title @a title {"text":"§c埋立完了!"}')
     await command_send_queue(f'title @a subtitle {{"text":"ありがとう! {user}"}}')
     await command_send_queue("bedrock fill")
     await asyncio.sleep(1)
+    print(minecraft_id)
 
-async def corgi_messege(user):
+async def corgi_messege(user,minecraft_id):
     await command_send_queue('title @a title {"text":"とにかく荒らせ!"}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
+    print(minecraft_id)
 
-async def mishka_storm(user):
+async def mishka_storm(user,minecraft_id):
     await command_send_queue('title @a title {"text":"§cTNTの嵐"}')
     await command_send_queue(f'title @a subtitle {{"text":"{user}"}}')
     await asyncio.sleep(3.20)
     for i in range(165):
         await command_send_queue("bedrock tnt 2")
         await asyncio.sleep(0.05)
+    print(minecraft_id)
 
 async def gift_counting(gift_times):
     global gift_counter
@@ -113,6 +129,8 @@ async def on_like_mod(event,streamer_ID):
     # print(event.count)
     user_id = event.user.unique_id
     user_like_total_count = event.count
+    minecraft_id = config.tiktok_to_minecraft[streamer_ID]
+
     # print("user:",user_id,"like count... ",user_like_total_count)
     # 個別カウント更新
     if user_id not in user_like_count:
@@ -137,15 +155,14 @@ async def on_like_mod(event,streamer_ID):
         selected = random.choices(
             config.minecraft_effects,
             weights=[e[2] for e in config.minecraft_effects],
+
             k=1
 
         )[0]
         print(selected)
         await command_send_queue(f'{selected[0]}')
-        await command_send_queue(f'title {config.player_name} title {{"text":"{selected[1]}"}}')
-        await command_send_queue(f'title @a subtitle {{"text":"{event.user.nickname}"}}')
-
-
+        await command_send_queue(f'title {minecraft_id} title {{"text":"{selected[1]}"}}')
+        await command_send_queue(f'title {minecraft_id} subtitle {{"text":"{event.user.nickname}"}}')
 
         user_like_count[user_id] -= USER_LIKE_THRESHOLD
 
@@ -169,10 +186,11 @@ async def on_like_mod(event,streamer_ID):
 already_triggered = set()
 async def on_follow_mod(event,streamer_ID):
     user_id = event.user.unique_id
+    minecraft_id = config.tiktok_to_minecraft[streamer_ID]
     if user_id not in already_triggered:
         already_triggered.add(user_id)
         # await command_send_queue(f"bedrock tnt 3 {event.user.nickname}")
-        await command_send_queue(f'title @a title {{"text":"§c{event.user.nickname}の試練"}}')
+        await command_send_queue(f'title {minecraft_id} title {{"text":"§c{event.user.nickname}の試練"}}')
         for i in range(5):
             monster = random.choices(
                 config.panic_monsters,
@@ -187,13 +205,14 @@ async def on_follow_mod(event,streamer_ID):
             z_offset = round(r * math.sin(theta))
             y_offset = 1  # プレイヤーの頭上1ブロック
 
-            await command_send_queue(f'execute at @r run summon {monster} ~{x_offset} ~{y_offset} ~{z_offset} {{CustomName:"\\"{event.user.nickname}の試験官\\""}}')
-        await command_send_queue(f'title @a subtitle {{"text":"{event.user.nickname}"}}')
+            await command_send_queue(f'execute at {minecraft_id} run summon {monster} ~{x_offset} ~{y_offset} ~{z_offset} {{CustomName:"\\"{event.user.nickname}の試験官\\""}}')
+        await command_send_queue(f'title {minecraft_id} subtitle {{"text":"{event.user.nickname}"}}')
+
 
 
 async def on_comment_mod(event,streamer_ID):
     now = datetime.now()
-    print(f"{event.user.nickname} >> {event.comment}"f" at {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f"{event.user.nickname} >> {event.comment} at {now.strftime('%Y-%m-%d %H:%M:%S')} form {streamer_ID}")
 
 
 async def add_time(seconds=300):
@@ -235,6 +254,8 @@ async def time_measurement():
         config.current_multiplier = 1
         config.time_measurement_running = False
         await command_send_queue(f"bossbar remove timer")
+
+
 async def on_gift_mod(event,streamer_ID):
     global gift_counter,coin_counter
 
@@ -245,6 +266,7 @@ async def on_gift_mod(event,streamer_ID):
     times = event.repeat_count
     #ログ用
     now = datetime.now()
+    minecraft_id = config.tiktok_to_minecraft[streamer_ID]
 
     print(f"{user} sent a {name} (x{times}) at {now.strftime('%Y-%m-%d %H:%M:%S')}")
     # streak 終了時のみ処理
@@ -253,30 +275,30 @@ async def on_gift_mod(event,streamer_ID):
         await coin_counting(coin,times)
 
 
-        print(f"{user} sent a {name} (x{times}) at {now.strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"{user} sent a {name} (x{times}) at {now.strftime('%Y-%m-%d %H:%M:%S {minecraft_id}')}")
         if name == "Heart Me":
-            asyncio.create_task(heart_me(user,times))
+            asyncio.create_task(heart_me(user,times,minecraft_id))
 
         elif name == "Rose":
-            asyncio.create_task(spawn_tnt(user, times))
+            asyncio.create_task(spawn_tnt(user, times,0.1,minecraft_id))
 
         elif name == "Finger Heart":
-            asyncio.create_task(spawn_multi_tnt(user, times, per=5,delay=0.1))
+            asyncio.create_task(spawn_multi_tnt(user, times, 5,0.1,minecraft_id))
 
         elif name == "Rosa":
-            asyncio.create_task(levitation_effect(user,times,delay=15))
+            asyncio.create_task(levitation_effect(user,times,15,minecraft_id))
 
         elif name == "BFF Necklace":
-            asyncio.create_task(summon_zombies(user, times))
+            asyncio.create_task(summon_zombies(user, times,minecraft_id))
 
         elif name == "Perfume":
-            asyncio.create_task(fill_blocks(user,times))
+            asyncio.create_task(fill_blocks(user,times,minecraft_id))
 
         elif name == "Doughnut":
-            asyncio.create_task(spawn_donuts_tnt(user, 2 * times, per=20, delay=2))
+            asyncio.create_task(spawn_donuts_tnt(user, 2 * times, 20, 2,minecraft_id))
 
         elif name == "Paper Crane":
-            asyncio.create_task(blank_info(user,name))
+            asyncio.create_task(blank_info(user,name,minecraft_id))
 
     elif not event.gift.streakable:
         await gift_counting(times)
@@ -284,16 +306,16 @@ async def on_gift_mod(event,streamer_ID):
 
 
         if name == "Hand Hearts":
-            asyncio.create_task(fill_area(user))
+            asyncio.create_task(fill_area(user,minecraft_id))
 
         elif name == "Mishka Bear":
-            asyncio.create_task(mishka_storm(user))
+            asyncio.create_task(mishka_storm(user,minecraft_id))
 
         elif name == "Corgi":
-            asyncio.create_task(corgi_messege(user))
+            asyncio.create_task(corgi_messege(user,minecraft_id))
 
         elif name == "Galaxy":
-            asyncio.create_task(blank_info(user,name))
+            asyncio.create_task(blank_info(user,name,minecraft_id))
 
     if 5000 <= coin_counter:
         while coin_counter > 5000:
